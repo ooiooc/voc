c33dba786039bfe7ffda4ace8a3de7ae65a086bd
ooiooc <shinjh1102@naver.com>
2022-01-27 23:06:18 +0900

    비즈니스 로직 추가 : panalty Confirm Status 등록

--- voc/src/main/java/com/project/voc/domain/Panalty.java
+++ voc/src/main/java/com/project/voc/domain/Panalty.java
@@ -1,12 +1,15 @@
 package com.project.voc.domain;
 
+import com.project.voc.domain.company.Carrier;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.*;
 
 @NoArgsConstructor
+@Setter
 @Entity
 public class Panalty {
 
@@ -14,8 +17,18 @@ public class Panalty {
     @GeneratedValue
     private Long id;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "voc_id")
-    private Voc panaltyInfo;
+    @OneToOne(mappedBy = "voc", fetch = LAZY)
+    private Voc voc;
+
+    // 패널티 확인여부
+    @Enumerated(EnumType.STRING)
+    private ConfirmStatus check;
+
+    // 확인여부 등록
+    public Panalty setConfirmStatus(Carrier carrier) {
+        Panalty panalty= new Panalty();
+        panalty.setCheck(ConfirmStatus.read);
+        return panalty;
+    }
 
 }
