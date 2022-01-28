64ff29807b481d38ab903be0c4db7b32931e292f
ooiooc <shinjh1102@naver.com>
2022-01-28 21:50:53 +0900

    entity 수정, 배상정보 등록 메서드 추가

--- voc/src/main/java/com/project/voc/domain/Indemnity.java
+++ voc/src/main/java/com/project/voc/domain/Indemnity.java
@@ -1,14 +1,20 @@
 package com.project.voc.domain;
 
+import com.project.voc.domain.company.Carrier;
+import com.project.voc.domain.company.Client;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 import static javax.persistence.FetchType.LAZY;
 
+@Getter
+@Setter
 @NoArgsConstructor
 @Entity
-public class Indemnity extends Voc {
+public class Indemnity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
@@ -18,9 +24,38 @@ public class Indemnity extends Voc {
     // 배상 금액
     private String cost;
 
+    // 운송회사
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "carrier_id")
+    private Carrier carrier;
+
+    // 고객사
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
     // VOC 정보
-    @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "voc_id")
-    private Voc vocInfo;
+    @OneToOne(mappedBy = "indemnity", fetch = LAZY)
+    private Voc voc;
+
+    // 패널티
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "panalty_id")
+    private Panalty panalty;
+
+    // 운송사 기사 배상정보 등록
+    // 이의제기 없으면(accept) -> 배상정보 등록
+    public Indemnity createIndemnity (Voc voc, Carrier carrier, Panalty panalty, String cost) {
+        Indemnity indemnity = new Indemnity();
+
+        if(panalty.getObjectionStatus() == ObjectionStatus.ACCEPT
+                && panalty.getCheck() == ConfirmStatus.READ) {
+            indemnity.setVoc(voc);
+            indemnity.setCarrier(carrier);
+            indemnity.setCost(cost);
+        }
+
+        return indemnity;
+    }
 
 }
