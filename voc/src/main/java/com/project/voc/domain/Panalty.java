6b29f3770d16e270a5ba99638a83935374efadfb
ooiooc <shinjh1102@naver.com>
2022-01-28 21:54:55 +0900

    패널티 발급 메서드 추가

--- voc/src/main/java/com/project/voc/domain/Panalty.java
+++ voc/src/main/java/com/project/voc/domain/Panalty.java
@@ -1,6 +1,7 @@
 package com.project.voc.domain;
 
 import com.project.voc.domain.company.Carrier;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
@@ -9,31 +10,47 @@ import javax.persistence.*;
 import static javax.persistence.FetchType.*;
 
 @NoArgsConstructor
+@Getter
 @Setter
 @Entity
-public class Panalty {
+public class Panalty extends BaseTimeEntity{
 
     @Id
     @GeneratedValue
     private Long id;
 
-    @OneToOne(mappedBy = "voc", fetch = LAZY)
-    private Voc voc;
+    // 패널티
+    private String cost;
 
     // 패널티 확인여부
     @Enumerated(EnumType.STRING)
     private ConfirmStatus check;
 
-    // 패널티 처리 상태
-    @Column(name = "panalty_status")
+    // 패널티 처리상태
+    //@Column(name = "panalty_status")
     @Enumerated(EnumType.STRING)
-    private PanaltyStatus status;
+    private PanaltyStatus status; // waiting, complete;
 
-    // 확인여부 등록
-    public Panalty setConfirmStatus(Carrier carrier) {
+    // 이의제기
+    @Enumerated(EnumType.STRING)
+    private ObjectionStatus objectionStatus;
+
+    @OneToOne(mappedBy = "voc", fetch = LAZY)
+    private Voc voc;
+
+    @ManyToOne
+    @JoinColumn(name = "carrier")
+    private Carrier carrier;
+
+    // 패널티 발급
+    public Panalty createPanalty(Carrier carrier, String cost) {
         Panalty panalty= new Panalty();
-        panalty.setCheck(ConfirmStatus.read);
+        panalty.setCarrier(carrier);
+        panalty.setCost(cost);
         return panalty;
     }
+
+    // 패널티
+
 
 }
