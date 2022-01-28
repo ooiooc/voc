39ae00f2eeafa52fd02d5a2c26b2111625487d9f
ooiooc <shinjh1102@naver.com>
2022-01-28 20:29:56 +0900

    create BaseTimeEntity

---
+++ voc/src/main/java/com/project/voc/domain/BaseTimeEntity.java
@@ -0,0 +1,25 @@
+package com.project.voc.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    // 생성일
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    // 수정일
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+}
