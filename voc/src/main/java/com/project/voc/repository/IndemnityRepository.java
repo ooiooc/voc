9021024f71b79270694bc95ea69944e873eb622b
ooiooc <shinjh1102@naver.com>
2022-01-28 20:30:11 +0900

    create IndemnityRepository

---
+++ voc/src/main/java/com/project/voc/repository/IndemnityRepository.java
@@ -0,0 +1,8 @@
+package com.project.voc.repository;
+
+import com.project.voc.domain.Indemnity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndemnityRepository extends JpaRepository<Indemnity,Long> {
+
+}
