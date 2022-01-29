8985e74d7449e65aa3bf93be4b02fd76a642801e
ooiooc <shinjh1102@naver.com>
2022-01-29 18:07:04 +0900

    voc 등록 메서드 추가

--- voc/src/main/java/com/project/voc/domain/Voc.java
+++ voc/src/main/java/com/project/voc/domain/Voc.java
@@ -1,6 +1,9 @@
 package com.project.voc.domain;
 
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
@@ -9,9 +12,10 @@ import java.util.List;
 
 import static javax.persistence.FetchType.LAZY;
 
+@Getter @Setter
 @NoArgsConstructor
 @Entity
-public class Voc {
+public class Voc extends BaseTimeEntity{
 
     @Id
     @GeneratedValue
@@ -22,15 +26,30 @@ public class Voc {
     private String name;
 
     // voc 내용
-    private String info;
+    private String vocInfo;
 
     // 패널티
-    @OneToMany(mappedBy = "panalty")
-    private List<Voc> voclist = new ArrayList<>();
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "panalty_id")
+    private Panalty panalty;
 
     // 배상금액
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "indemnity_id")
-    private Indemnity cost;
+    private Indemnity indemnity;
+
+    // 연관관계
+
+
+    // voc 등록
+    public static Voc createVoc(Panalty panalty, Indemnity indemnity, Code code) {
+        Voc voc = new Voc();
+
+        voc.setPanalty(panalty);
+        voc.setIndemnity(indemnity);
+        return voc;
+    }
+
+
 
 }
