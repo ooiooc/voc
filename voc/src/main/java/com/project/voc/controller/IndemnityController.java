fa33b872ec03531e04ee4f3ab7cc9b1b201f913d
ooiooc <shinjh1102@naver.com>
2022-01-31 00:25:00 +0900

    배상목록 controller 수정

--- voc/src/main/java/com/project/voc/controller/IndemnityController.java
+++ voc/src/main/java/com/project/voc/controller/IndemnityController.java
@@ -1,6 +1,8 @@
 package com.project.voc.controller;
 
 import com.project.voc.domain.Indemnity;
+import com.project.voc.dto.IndemnityRequestDto;
+import com.project.voc.dto.IndemnityResponseDto;
 import com.project.voc.service.IndemnityService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Api(value = "배상정보 API")
@@ -23,17 +26,21 @@ public class IndemnityController {
     // 배상정보 등록
     @ApiOperation(value = "배상 정보 등록")
     @PostMapping("api/indemnity")
-    public ResponseEntity<Indemnity> createIndemnity(@RequestBody Indemnity indemnity) {
-        return ResponseEntity.ok().body(indemnityService.saveIndemnity(indemnity));
+    public ResponseEntity<Indemnity> createIndemnity(@RequestBody IndemnityRequestDto requestDto) {
+        return ResponseEntity.ok().body(indemnityService.saveIndemnity(requestDto));
     }
 
     // 배상목록
     @ApiOperation(value = "배상목록 조회")
     @GetMapping("/api/indemnity")
-    //public ResponseEntity<List<dto명>> vocList() throws Exception {
-    public ResponseEntity<List<Indemnity>> vocList() throws Exception {
+    public ResponseEntity<List<IndemnityResponseDto>> vocList() throws Exception {
         List<Indemnity> indemnities = indemnityService.findList();
-        return ResponseEntity.ok().body(indemnities);
+        List<IndemnityResponseDto> dtoList = new ArrayList<>();
+        for (Indemnity i : indemnities) {
+            IndemnityResponseDto dto = new IndemnityResponseDto(i);
+            dtoList.add(dto);
+        }
+        return ResponseEntity.ok().body(dtoList);
     }
 
 }
