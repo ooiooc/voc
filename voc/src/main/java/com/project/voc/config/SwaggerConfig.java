430be28d01d517c6b06661246c2ecb262d870fe5
ooiooc <shinjh1102@naver.com>
2022-01-31 00:19:03 +0900

    swaggerconfig 수정

--- voc/src/main/java/com/project/voc/config/SwaggerConfig.java
+++ voc/src/main/java/com/project/voc/config/SwaggerConfig.java
@@ -2,24 +2,21 @@ package com.project.voc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig{
+public class SwaggerConfig {
 
     @Bean
     public Docket api(){
@@ -28,7 +25,7 @@ public class SwaggerConfig{
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
                 .apiInfo(apiInfo()).select()
-                .apis(RequestHandlerSelectors.basePackage("com.project.voc.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
@@ -49,7 +46,6 @@ public class SwaggerConfig{
         return new ApiInfoBuilder()
                 .title("VOC API")
                 .description("VOC REST API")
-                //.contact(new Contact("Jeonghye Shin", "https://github.com/ooiooc", "shinjh1102@naver.com"))
                 .build();
     }
 
