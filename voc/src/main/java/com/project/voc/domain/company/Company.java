82e6651c902ac61c7cab41b17cea802df52982a8
ooiooc <shinjh1102@naver.com>
2022-01-31 00:23:23 +0900

    api 옵션 설정 / 연관관계 매핑

--- voc/src/main/java/com/project/voc/domain/company/Company.java
+++ voc/src/main/java/com/project/voc/domain/company/Company.java
@@ -1,25 +1,35 @@
 package com.project.voc.domain.company;
 
 import com.project.voc.domain.Code;
+import com.project.voc.domain.Voc;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
+@Getter @Setter
 @NoArgsConstructor
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
 @Entity
 public abstract class Company {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
+    @ApiModelProperty(value = "회사 id")
     @Column(name = "company_id")
     private Long id;
 
-    @Column(name = "company_name")
-    private String name;
-
+    @ApiModelProperty(value = "회사 분류")
     @Column(name = "company_type")
     private String type;
+
+    @OneToMany(mappedBy = "company", fetch = LAZY)
+    private List<Voc> vocList = new ArrayList<>();
 
 }
