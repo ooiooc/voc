b26d8988a4160019024fdb5d9965f979a667a8ca
ooiooc <shinjh1102@naver.com>
2022-01-30 11:15:02 +0900

    create indemnity repository

---
+++ voc/src/main/java/com/project/voc/service/IndemnityService.java
@@ -0,0 +1,28 @@
+package com.project.voc.service;
+
+import com.project.voc.domain.Indemnity;
+import com.project.voc.repository.IndemnityRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class IndemnityService {
+
+    public final IndemnityRepository indemnityRepository;
+
+    // 배상정보 등록
+    @Transactional
+    public void saveIndemnity(Indemnity indemnity) {
+        indemnityRepository.save(indemnity);
+    }
+
+    // 배상 목록 조회
+    public List<Indemnity> findItem() {
+        return indemnityRepository.findAll();
+    }
+
+}
